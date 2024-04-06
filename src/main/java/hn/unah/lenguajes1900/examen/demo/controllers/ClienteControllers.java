f011c735a4422885b0d86469b18a4a00f5106cae
MarvinAguilera <aguileram712@gmail.com>
2024-04-05 19:16:55 -0600

    segundaEdit

--- src/main/java/hn/unah/lenguajes1900/examen/demo/controllers/ClienteControllers.java
+++ src/main/java/hn/unah/lenguajes1900/examen/demo/controllers/ClienteControllers.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +29,7 @@ public class ClienteControllers {
     }
 
     @GetMapping("/buscar/cliente")
-    public Optional<Cliente> obtenerPorId(@RequestParam String dni) {
+    public Optional<Cliente> obtenerPorId(@PathVariable String dni) {
         return this.clienteServiceImpl.buscarClientePorId(dni);
     }
 
